cfc5863bafe59c99976c527057088179cd594f31
elvinsadikhov <elvin.279.6s@gmail.com>
2023-04-18 12:44:14 +0400

    made flutter_gen visible

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_az.dart
@@ -0,0 +1,21 @@
+import 'app_localizations.dart';
+
+/// The translations for Azerbaijani (`az`).
+class AppLocalizationsAz extends AppLocalizations {
+  AppLocalizationsAz([String locale = 'az']) : super(locale);
+
+  @override
+  String get hour_short => 'h';
+
+  @override
+  String get minute_short => 'm';
+
+  @override
+  String get change_year => 'Change Year';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get okay => 'Okay';
+}
---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_ru.dart
@@ -0,0 +1,21 @@
+import 'app_localizations.dart';
+
+/// The translations for Russian (`ru`).
+class AppLocalizationsRu extends AppLocalizations {
+  AppLocalizationsRu([String locale = 'ru']) : super(locale);
+
+  @override
+  String get hour_short => 'h';
+
+  @override
+  String get minute_short => 'm';
+
+  @override
+  String get change_year => 'Change Year';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get okay => 'Okay';
+}
