49060a800b3f75d85a8d0efb72c2729a4dec3d26
elvinsadikhov <elvin.279.6s@gmail.com>
2024-02-08 22:20:05 +0400

    patch: upgraded intl to 0.18.0

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,21 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get hour_short => 'h';
+
+  @override
+  String get minute_short => 'm';
+
+  @override
+  String get change_year => 'Change Year';
+
+  @override
+  String get cancel => 'Cancel';
+
+  @override
+  String get okay => 'Okay';
+}
